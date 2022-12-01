17ea9f811786b0532be9a72847b9d1881e01dbf7
Robert Surton <rahpaere@gmail.com>
2022-12-01 04:51:09 +0000

    Set up readme and template.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# aoc2022
-Advent of Code 2022
+# Advent of Code 2022
+
+My second Advent of Code, again mostly exploring modern C++.
---
+++ template/part2.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <bitset>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <variant>
+#include <vector>
+
+using namespace std;
+
+int main() {
+}
