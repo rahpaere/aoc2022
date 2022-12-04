fdb3c73eb53a6c4269c8d08b154df570303a430a
Robert Surton <rahpaere@gmail.com>
2022-12-04 05:18:19 +0000

    Day 4

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ see what ranges can do.
 
 1. [Calorie Counting](https://adventofcode.com/2022/day/1)
 2. [Rock Paper Scissors](https://adventofcode.com/2022/day/2)
-2. [Rucksack Reorganization](https://adventofcode.com/2022/day/3)
+3. [Rucksack Reorganization](https://adventofcode.com/2022/day/3)
+4. [Camp Cleanup](https://adventofcode.com/2022/day/4)
---
+++ day04/part1.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <bitset>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <variant>
+#include <vector>
+
+using namespace std;
+
+struct Assignment {
+	int from;
+	int to;
+};
+
+struct Pair {
+	Assignment first;
+	Assignment other;
+};
+
+istream &operator>>(istream &in, Assignment &a) {
+	char hyphen;
+	return in >> a.from >> hyphen >> a.to;
+}
+
+istream &operator>>(istream &in, Pair &p) {
+	char comma;
+	return in >> p.first >> comma >> p.other;
+}
+
+ostream &operator<<(ostream &out, const Assignment &a) {
+	return out << a.from << '-' << a.to;
+}
+
+ostream &operator<<(ostream &out, const Pair &p) {
+	return out << p.first << ',' << p.other;
+}
+
+bool contains(const Pair &p) {
+	return    (p.first.from >= p.other.from && p.first.to <= p.other.to)
+	       || (p.first.from <= p.other.from && p.first.to >= p.other.to);
+}
+
+int main() {
+	vector<Pair> pairs;
+	copy(istream_iterator<Pair>(cin), {}, back_inserter(pairs));
+	cout << count_if(pairs.begin(), pairs.end(), contains) << endl;
+}
